0742269cef60cc4443bfaa896e51a73690293413
Richard Peck <rpeck@frontlineutilities.co.uk>
2019-07-08 12:23:23 +0100

    Urtiden

--- config/routes.rb
+++ config/routes.rb
@@ -19,8 +19,17 @@
 ## Routes ##
 ## Since we are using the ShopifyApp gem, we are able to integrate directly ##
 Rails.application.routes.draw do
-  root to: 'home#index' # => ShopifyApp (this is used to show the "authenticated" page)
+
+  # => Shopify App
+  # => This creates a registration/login system for Shopify's oAuth API
+  # => Allows us to create an app which integrates with Shopify
+  root to: 'products#index'         # => ShopifyApp (this is used to show the "authenticated" page)
   mount ShopifyApp::Engine, at: '/' # => ShopifyApp Engine (required for oAuth etc)
+
+  # => Functionality
+  # => This allows us to manage the various products within the system
+  resources :products, only: [:index, :update, :show], path_name: "" # => Allows us to manage products within the Shopify store 
+
 end
 
 ########################################
