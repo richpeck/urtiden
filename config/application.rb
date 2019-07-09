da3094d16f322af1351b9a1c71cb860e710a5459
Richard Peck <rpeck@frontlineutilities.co.uk>
2019-07-09 12:30:48 +0100

    Database Adapter Test

--- config/application.rb
+++ config/application.rb
@@ -34,8 +34,8 @@ module Urtiden
 
     # => DataTables
     # => https://github.com/jbox-web/ajax-datatables-rails#configuration
-    AjaxDatatablesRails.configure do |config|
-      config.db_adapter = :pg
+    AjaxDatatablesRails.configure do |c|
+      c.db_adapter = config.database_configuration[Rails.env]['adapter'].to_sym
     end
 
   end
