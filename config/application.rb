69af8600ead25b7fbd194470c2874a6606f9466f
Richard Peck <rpeck@frontlineutilities.co.uk>
2019-07-14 12:50:55 +0100

    Sidekiq

--- config/application.rb
+++ config/application.rb
@@ -25,27 +25,57 @@ Bundler.require(*Rails.groups)
 module Urtiden
   class Application < Rails::Application
 
-    # => Rails 6.0
-    # => Allows us to use all the defaults etc
-    config.load_defaults 6.0
+    ##########################################
+    ##########################################
 
-    # => Locale
-    # => https://stackoverflow.com/questions/6118779/how-to-change-default-timezone-for-active-record-in-rails
-    config.time_zone = "London"
-    config.active_record.default_timezone = :local
+      # => Rails 6.0
+      # => Allows us to use all the defaults etc
+      config.load_defaults 6.0
 
-    # => DataTables
-    # => https://github.com/jbox-web/ajax-datatables-rails#configuration
-    AjaxDatatablesRails.configure do |config|
-      config.db_adapter = Rails.env.staging? ? :mysql2 : Rails.configuration.database_configuration[Rails.env]['adapter'].to_sym # => Need to fix this -- Heroku eager loads everything
-    end
+      # => Locale
+      # => https://stackoverflow.com/questions/6118779/how-to-change-default-timezone-for-active-record-in-rails
+      config.time_zone = "London"
+      config.active_record.default_timezone = :local
 
-    # => Assets
-    # => Allows us to use helper methods inside JS
-    # => Requires sprockets-helpers gem (https://stackoverflow.com/a/37092476/1143732)
-    # => https://stackoverflow.com/questions/7451517/using-a-rails-helper-method-within-a-javascript-asset
-    Sprockets::Context.send :include, Rails.application.routes.url_helpers
-    Sprockets::Context.send :include, ActionView::Helpers
+      # => DataTables
+      # => https://github.com/jbox-web/ajax-datatables-rails#configuration
+      AjaxDatatablesRails.configure do |config|
+        config.db_adapter = Rails.env.staging? ? :mysql2 : Rails.configuration.database_configuration[Rails.env]['adapter'].to_sym # => Need to fix this -- Heroku eager loads everything
+      end
+
+      # => Assets
+      # => Allows us to use helper methods inside JS
+      # => Requires sprockets-helpers gem (https://stackoverflow.com/a/37092476/1143732)
+      # => https://stackoverflow.com/questions/7451517/using-a-rails-helper-method-within-a-javascript-asset
+      Sprockets::Context.send :include, Rails.application.routes.url_helpers
+      Sprockets::Context.send :include, ActionView::Helpers
+
+    ##########################################
+    ##########################################
+
+      # => ActiveJob
+      # => Allows us to manage the queue for the "sync all" method
+      config.active_job.queue_adapter = :sidekiq
+
+      # => ActiveJob Throttle
+      # => Allows us to manage the underlying rate limiting of API requests
+      ActiveJob::TrafficControl.client = Redis.new( host: Rails.application.credentials.dig(Rails.env.to_sym, :redis, :host), port: Rails.application.credentials.dig(Rails.env.to_sym, :redis, :port), password: Rails.application.credentials.dig(Rails.env.to_sym, :redis, :pass))
+
+      # => Redis
+      # => Hosted at RedisLabs
+      # => Both settings required to get it working
+      Sidekiq.configure_client do |config|
+        config.redis = { url: "redis://#{Rails.application.credentials.dig(Rails.env.to_sym, :redis, :host)}:#{Rails.application.credentials.dig(Rails.env.to_sym, :redis, :port)}", password:  Rails.application.credentials.dig(Rails.env.to_sym, :redis, :pass) }
+      end
+
+      # => Server
+      # => Allows us to connect to the right server
+      Sidekiq.configure_server do |config|
+        config.redis = { url: "redis://#{Rails.application.credentials.dig(Rails.env.to_sym, :redis, :host)}:#{Rails.application.credentials.dig(Rails.env.to_sym, :redis, :port)}", password:  Rails.application.credentials.dig(Rails.env.to_sym, :redis, :pass) }
+      end
+
+    ##########################################
+    ##########################################
 
   end
 end
---
+++ config/schedule.rb
@@ -0,0 +1,35 @@
+# Use this file to easily define all of your cron jobs.
+#
+# It's helpful, but not entirely necessary to understand cron before proceeding.
+# http://en.wikipedia.org/wiki/Cron
+
+# Example:
+#
+# set :output, "/path/to/my/cron_log.log"
+#
+# every 2.hours do
+#   command "/usr/bin/some_great_command"
+#   runner "MyModel.some_method"
+#   rake "some:great:rake:task"
+# end
+#
+# every 4.days do
+#   runner "AnotherModel.prune_old_records"
+# end
+
+# Learn more: http://github.com/javan/whenever
+
+# => https://gist.github.com/mdesantis/5356195
+set :env_path, '"$HOME/.rbenv/shims":"$HOME/.rbenv/bin"'
+
+# doesn't need modifications
+# job_type :command, ":task :output"
+
+job_type :rake,   %q{ cd :path && PATH=:env_path:"$PATH" RAILS_ENV=:environment bin/rake :task --silent :output }
+job_type :runner, %q{ cd :path && PATH=:env_path:"$PATH" rails runner -e :environment ':task' :output }
+job_type :script, %q{ cd :path && PATH=:env_path:"$PATH" RAILS_ENV=:environment bundle exec bin/:task :output }
+
+every :hour do # 1.minute 1.day 1.week 1.month 1.year is also supported
+  runner "Product.download_csv" # => Should be in the Activeadmin area but had to include here to ensure it worekd
+  runner "Product.sync_all unless Product.queue_size > 0"     # => Allows us to queue products in Sidekiq queue
+end
