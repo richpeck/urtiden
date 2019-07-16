e4fb645c435223c1653fd4354dc1bbecf1d91da9
Richard Peck <rpeck@frontlineutilities.co.uk>
2019-07-16 12:37:00 +0100

    Product Count

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -82,7 +82,7 @@ class Shop < ActiveRecord::Base
       end
 
       ## Return ##
-      return CSV.read(raw.file, headers: true).count || products
+      return CSV.read(raw.file, headers: true) || products
 
     end
 
