9c1871c189a4e447eaf4d6892f692f77bbcd85f7
Richard Peck <rpeck@frontlineutilities.co.uk>
2019-07-16 12:46:07 +0100

    Raw file path

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -76,7 +76,7 @@ class Shop < ActiveRecord::Base
         ## Show response (might be huge) ##
         ## This is where we should put all the products into the local db ##
         ## Converts allow us to change the "attributes" column to attribs - https://stackoverflow.com/a/37059741/1143732 ##
-        CSV.foreach(csv, headers: :first_row, col_sep: ";", header_converters: lambda { |name| {"attributes" => "attribs"}.fetch(name, name).to_sym }) do |product|
+        CSV.foreach(raw.file.path, headers: :first_row, col_sep: ";", header_converters: lambda { |name| {"attributes" => "attribs"}.fetch(name, name).to_sym }) do |product|
           new_products << @shop.products.new(product.to_h)
         end
 
